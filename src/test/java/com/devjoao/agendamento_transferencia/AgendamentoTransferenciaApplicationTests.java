f6b60d4e9987dc90c4f5d553454523ba6c70bb8e
João <joaociardullo@hotmail.com>
2025-05-04 20:33:08 -0300

    feat: first init commit

---
+++ src/test/java/com/devjoao/agendamento_transferencia/AgendamentoTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoao.agendamento_transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
