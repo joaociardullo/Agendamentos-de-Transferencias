56d9b27d4982ca62a201c87eb76f8398de921032
João <joaociardullo@hotmail.com>
2025-05-04 20:35:54 -0300

    feat: criando o mapper

---
+++ src/main/java/com/devjoao/agendamento_transferencia/mapper/TransferenciaMapper.java
@@ -0,0 +1,12 @@
+package com.devjoao.agendamento_transferencia.mapper;
+
+import com.devjoao.agendamento_transferencia.domain.Entity.Transferencia;
+import com.devjoao.agendamento_transferencia.domain.dto.TransferenciaDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TransferenciaMapper {
+    Transferencia toEntity(TransferenciaDTO dto);
+
+    //TransferenciaDTO toDto(Transferencia entity);
+}
