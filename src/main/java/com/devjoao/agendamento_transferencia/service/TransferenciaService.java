73ba22cfd69404574ba518060811ad6662d64002
João <joaociardullo@hotmail.com>
2025-05-04 20:36:34 -0300

    feat: criando a interface e o servico da aplicação

---
+++ src/main/java/com/devjoao/agendamento_transferencia/service/TransferenciaService.java
@@ -0,0 +1,13 @@
+package com.devjoao.agendamento_transferencia.service;
+
+import com.devjoao.agendamento_transferencia.domain.Entity.Transferencia;
+import com.devjoao.agendamento_transferencia.domain.dto.TransferenciaDTO;
+
+import java.util.List;
+
+public interface TransferenciaService {
+
+    Transferencia agendar(TransferenciaDTO transferenciaDTO);
+
+    List<Transferencia> listarTodas();
+}
---
+++ src/main/java/com/devjoao/agendamento_transferencia/service/impl/TransferenciaServiceImpl.java
@@ -0,0 +1,42 @@
+package com.devjoao.agendamento_transferencia.service.impl;
+
+import com.devjoao.agendamento_transferencia.Strategy.TaxaCalculatorService;
+import com.devjoao.agendamento_transferencia.domain.Entity.Transferencia;
+import com.devjoao.agendamento_transferencia.domain.dto.TransferenciaDTO;
+import com.devjoao.agendamento_transferencia.mapper.TransferenciaMapper;
+import com.devjoao.agendamento_transferencia.repository.TransferenciaRepository;
+import com.devjoao.agendamento_transferencia.service.TransferenciaService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+public class TransferenciaServiceImpl implements TransferenciaService {
+
+    private final TransferenciaRepository repository;
+    private final TransferenciaMapper mapper;
+    private final TaxaCalculatorService taxaCalculator;
+
+    public TransferenciaServiceImpl(TransferenciaRepository repository, TransferenciaMapper mapper, TaxaCalculatorService taxaCalculator) {
+        this.repository = repository;
+        this.mapper = mapper;
+        this.taxaCalculator = taxaCalculator;
+    }
+
+    public Transferencia agendar(TransferenciaDTO transferenciaDTO) {
+        var dias = ChronoUnit.DAYS.between(LocalDate.now(), transferenciaDTO.getDataTransferencia());
+
+        BigDecimal taxa = taxaCalculator.calculate(dias, transferenciaDTO.getValor());
+        transferenciaDTO.setTaxa(taxa);
+        transferenciaDTO.setDataAgendamento(LocalDate.now());
+        Transferencia entity = mapper.toEntity(transferenciaDTO);
+        return repository.save(entity);
+    }
+
+    public List<Transferencia> listarTodas() {
+        return repository.findAll();
+    }
+}
