afef25be3f4f027815972d67d9c04c158a098920
João <joaociardullo@hotmail.com>
2025-05-05 23:37:46 -0300

    feat: permitindo todas as tranferencias do metodo http no cors da aplicação

--- src/main/java/com/devjoao/agendamento_transferencia/config/CorsConfig.java
+++ src/main/java/com/devjoao/agendamento_transferencia/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200") // URL do seu frontend
-                        .allowedMethods("GET", "POST")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
